b244acd3a0f0548056884fcead0f1934f075068b
t4man <timothywforman@gmail.com>
2016-02-19 17:18:18 -0800

    refactor to remove for loops

--- scripts/selector.js
+++ scripts/selector.js
@@ -3,7 +3,7 @@ var selectorView = {};
 var template = Handlebars.compile($('#question-template').html());
 var temp = template(questions);
 $('div.questionBox').append(temp);
-//$('div.questionBox').hide();
+$('div.questionBox').hide();
 
 $('#selectorButton').click(function(){
   $('.breedArticles').hide();
@@ -14,75 +14,75 @@ answers = [];
 matches = [];
 
 selectorView.collect = function() {
-  $('#submitButton').click(function(event){
+  $('#submitButton').click(function(e){
     var selected = $('input[type="radio"]:checked').each(function(){
     var userInput = parseInt($(this).attr("value").toString(), 10);
     answers.push(userInput);
-    }); event.preventDefault();
+    }); e.preventDefault();
 
-    for (i=0; i<catProperties.length; i++){
+    $('catProperties').each(function(i){
     var aff = catProperties[i].affectionate;
     var affBreed = catProperties[i].breed;
       if (answers[0] === aff){
       matches.push(affBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var groom = catProperties[i].grooming;
     var groomBreed = catProperties[i].breed;
       if (answers[1] === groom){
       matches.push(groomBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var shed = catProperties[i].shedding;
     var shedBreed = catProperties[i].breed;
       if (answers[2] === shed){
       matches.push(shedBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var play = catProperties[i].playfulness;
     var playBreed = catProperties[i].breed;
       if (answers[3] === play){
       matches.push(playBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var intel = catProperties[i].intelligence;
     var intelBreed = catProperties[i].breed;
       if (answers[4] === intel){
       matches.push(intelBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var vocal = catProperties[i].vocality;
     var vocalBreed = catProperties[i].breed;
       if (answers[5] === vocal){
       matches.push(vocalBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var toler = catProperties[i].toleranceWithKids;
     var tolerBreed = catProperties[i].breed;
       if (answers[6] === toler){
       matches.push(tolerBreed);
       }
-    }
-    for (i=0; i<catProperties.length; i++){
+    });
+    $('catProperties').each(function(i){
     var health = catProperties[i].generalHealth;
     var healthBreed = catProperties[i].breed;
       if (answers[7] === health){
       matches.push(healthBreed);
       }
-    }
+    });
     $('.questionBox').hide();
     $('article.allBreeds').hide();
 
-    for (i=0; i<catProperties.length; i++){
+    $('catProperties').each(function(i){
     var name = catProperties[i].breed;
       getCount(name, matches);
-    }
+    });
   });
 };
 function getCount(word, arr) {
@@ -97,5 +97,3 @@ function getCount(word, arr) {
 }
 
 selectorView.collect();
-
-
