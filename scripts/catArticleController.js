70c77ef4d976e8f1ec1286c5ae0dfed8d9cfdd83
t4man <timothywforman@gmail.com>
2016-02-22 15:31:39 -0800

    create filter for characteristic

--- scripts/catArticleController.js
+++ scripts/catArticleController.js
@@ -1,19 +1,16 @@
 (function(module) {
+
   var catArticlesController = {};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $('#char-filter').on('change', function() {
+      var attr = $(this).val();
+        $('.breed-articles').empty();
+        CatConstr.all.filter(function(o){
+          return o[attr] === 5;
+        }).map(function(a){
+          $('.breed-articles').append(a.toHtml());
+        });
+      });
 
   module.catArticlesController = catArticlesController;
 })(window);
