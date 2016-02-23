2744fed49c861a7cf819a860a474049c7edac6b4
Lex Myers <lex.myers@gmail.com>
2016-02-23 11:31:33 -0800

    connected shelter script to navController;' added geoloc

--- scripts/shelter.js
+++ scripts/shelter.js
@@ -6,22 +6,53 @@ function Shelter(opts) {
 
 Shelter.all = [];
 
-Shelter.requestShelterList = function(location, callback) {
-  var petFinderKey = 'fc112f63a02888e709b52b7778826df7';
-  var petFinderApi = 'http://api.petfinder.com/shelter.find?format=json&key='
+var map,
+    infowindow;
+
+function initShelterMap() {
+  var burien = {lat: 47.466575, lng: -122.341207};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: burien,
+    zoom: 11
+  });
+  var infowindow = new google.maps.InfoWindow();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log(pos);
+
+      infowindow.setPosition(pos);
+      infowindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  Shelter.requestShelterList = function(location, callback) {
+    var petFinderKey = 'fc112f63a02888e709b52b7778826df7';
+    var petFinderApi = 'http://api.petfinder.com/shelter.find?format=json&key='
     + petFinderKey + '&location=' + location +'&callback=?';
-  $.getJSON(petFinderApi)
+    $.getJSON(petFinderApi)
     .done(function(petApiData) {callback(petApiData);})
     .error(function(err) {console.log('Error: ' + JSON.stringify(err));});
-}
+  }
 
-Shelter.requestShelterList('98026', function(data) {
-  var shelterList = data.petfinder.shelters.shelter;
-  Shelter.loadAll(shelterList);
-});
+  Shelter.requestShelterList('98026', function(data) {
+    var shelterList = data.petfinder.shelters.shelter;
+    Shelter.loadAll(shelterList);
+  });
 
-Shelter.loadAll = function(list) {
-  Shelter.all = list.map(function(ele) {
-    return new Shelter(ele);
-  })
+  Shelter.loadAll = function(list) {
+    Shelter.all = list.map(function(ele) {
+      return new Shelter(ele);
+    })
+  }
+
 }
