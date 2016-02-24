6075774718fe4f2381c802eb2a6a26bd3f5de7a5
Lex Myers <lex.myers@gmail.com>
2016-02-24 12:50:37 -0800

    encapsulated curLocation

--- scripts/locate.js
+++ scripts/locate.js
@@ -1,17 +1,21 @@
-curLocation = {};
+(function(module) {
 
-curLocation.getLocation = function(callback) {
-  var pos;
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log('lat ' + position.coords.latitude);
-      console.log('lng ' + position.coords.longitude);
-      curLocation.pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      callback();
-    });
+  curLocation = {};
 
+  curLocation.getLocation = function(callback) {
+    var pos;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log('lat ' + position.coords.latitude);
+        console.log('lng ' + position.coords.longitude);
+        curLocation.pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        callback();
+      });
+    }
   }
-}
+
+  module.curLocation = curLocation;
+})(window);
