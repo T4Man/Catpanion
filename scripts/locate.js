011d8ddca587dff9d47a3dcd2de801807e23a07d
Lex Myers <lex.myers@gmail.com>
2016-02-24 14:41:40 -0800

    now can input zip manually

--- scripts/locate.js
+++ scripts/locate.js
@@ -1,20 +1,46 @@
 (function(module) {
-
   curLocation = {};
+
+  curLocation.getLocFromZip = function() {
+    curLocation.zipCode = prompt('Please enter your Zip Code', '98019');
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: curLocation.zipCode }, function(results, status) {
+      if(status === 'OK') {
+        curLocation.gLocation = results[0].geometry.location;
+        curLocation.latitude = curLocation.gLocation.lat();
+        curLocation.longitude = curLocation.gLocation.lng();
+        curLocation.pos = {
+          lat: curLocation.latitude,
+          lng: curLocation.longitude
+        };
+      } else {
+        console.log('geocode Not OK');
+      }
+    });
+  }
+
+  curLocation.getLocFromBrowser = function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      curLocation.latitude = position.coords.latitude;
+      curLocation.longitude = position.coords.longitude;
+      curLocation.pos = {
+        lat: curLocation.latitude,
+        lng: curLocation.longitude
+      };
+      curLocation.gLocation = new google.maps.LatLng(curLocation.latitude, curLocation.longitude);
+    });
+  }
 
   curLocation.getLocation = function(callback) {
     var pos;
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        console.log('lat ' + position.coords.latitude);
-        console.log('lng ' + position.coords.longitude);
-        curLocation.pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        callback();
-      });
-    }
+      curLocation.getLocFromBrowser();
+    } else {
+      curLocation.getLocFromZip();
+    };
+    if(callback) {
+      callback();
+    };
   }
 
   module.curLocation = curLocation;
