f0205102337375ab1318983aa43c5e929f0f4174
Lex Myers <lex.myers@gmail.com>
2016-02-25 14:00:28 -0800

    Thanks to Brian and Rick

--- scripts/breed.js
+++ scripts/breed.js
@@ -10,18 +10,21 @@ Breed.all = [];
 Breed.requestBreeds = function(callback) {
   var petFinderKey = 'fc112f63a02888e709b52b7778826df7';
   var petFinderApi = 'http://api.petfinder.com/breed.list?format=json&key=' + petFinderKey + '&animal=cat&location=98026&callback=?'
-  $.getJSON(petFinderApi)
-    .done(function(petApiData) {callback(petApiData);})
+  $.getJSON(petFinderApi, function(petApiData) { callback(petApiData); })
     .error(function(err) {console.log('Error: ' + JSON.stringify(err));});
 }
 
 Breed.loadAll = function(list) {
   Breed.all = list.map(function(ele) {
     return ele.$t
-  })
+  });
 }
 
 Breed.requestBreeds(function(data){
   var breedList = data.petfinder.breeds.breed;
   Breed.loadAll(breedList);
+
+  Breed.all.forEach(function(breed) {
+    $('#breeds').append($('<li>').text(breed));
+  });
 });
