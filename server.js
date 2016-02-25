0014352c7cdbd91a35d8ee366ade2256dd2669d0
t4man <timothywforman@gmail.com>
2016-02-25 12:50:36 -0800

    server.js

--- server.js
+++ server.js
@@ -3,31 +3,22 @@ var requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
-/*var proxyGoogle = function(request, response) {
+var proxyGoogle = function(request, response) {
   console.log('Routing Google request for', request.params[0]);
   (requestProxy({
     url: 'https://maps.googleapis.com/maps/api/place/details/json' + request.params[0],
     headers: { Authorization: 'token ' + process.env.GOOGLE_TOKEN }
   }))(request, response);
   };
-*/
+
 
 var requestProxy = require('express-request-proxy');
 
-app.get('/pfapi/pets', requestProxy({
-    url: 'http://api.petfinder.com/pet.find',
-    query: {
-      key: config.pfAPI,
-      format: 'json',
-      output: 'full'
-    }
-}));
-
-/*app.get('*', function(request, response) {
+app.get('*', function(request, response) {
   console.log('New request:', request.url);
   response.sendFile('index.html', { root: '.' });
 });
-*/
+
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
