b10762000fb335e94dd78faa4cf6f2b4b26ac8c2
t4man <timothywforman@gmail.com>
2016-02-25 21:04:05 -0800

    add client side routing

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,29 +1,3 @@
-// page('/', startPage.index);
+page('/', startPage.index);
 
-// page('/adoption',
-  // );
-  //shelterView.index);
-
-// page('/donate')
-  //initMap(),
-  //navController.index);
-
-// page('/clinics')
-  //initMap(),
-  //navController.index);
-
-// page('/nanny')
-  //initMap(),
-  //navController.index);
-
-// page('/Cat-Selector',
-  // selectorView.index);
-
-// page('/allBreeds',
-  //showAllCats(),
-  // CatConstr.index);
-
-// page('/filterCharacteristics',
-  // catArticlesController.index);
-
-// page();
+page();
